e19eb60d134026c3994ffa3705d8c08e7eb5a5a1
Piotr Henryk Dabrowski <phd@phd.re>
2023-03-26 21:21:21 +0200

    upload to git repo

---
+++ copy-packages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+#export PATH="${HOME}/firefox-ppa/:$PATH"
+export BROWSER=echo
+export LP_CREDENTIALS_FILE="${HOME}/launchpad.auth"
+
+cd $(dirname "$0")
+
+. config
+
+echo '[jammy 22.04]'
+./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=jammy   firefox 2>&1 || true
+
+echo '[kinetic 22.10]'
+./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=kinetic firefox 2>&1 || true
+
+echo '[lunar 23.04]'
+./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=lunar   firefox 2>&1 || true
