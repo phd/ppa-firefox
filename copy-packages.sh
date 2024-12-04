7353263cd76413da66ff6ad97b6a2837587d163b
Piotr Henryk Dabrowski <phd@phd.re>
2024-12-04 20:51:46 +0100

    removed Ubuntu 22.10 kinetic, 23.04 lunar, 23.10 mantic
    "kinetic is obsolete and will not accept new uploads."
    "lunar is obsolete and will not accept new uploads."
    "mantic is obsolete and will not accept new uploads."

--- copy-packages.sh
+++ copy-packages.sh
@@ -10,15 +10,6 @@ cd $(dirname "$0")
 echo '[jammy 22.04]'
 ./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=jammy    firefox 2>&1 || true
 
-echo '[kinetic 22.10]'
-./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=kinetic  firefox 2>&1 || true
-
-echo '[lunar 23.04]'
-./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=lunar    firefox 2>&1 || true
-
-echo '[mantic 23.10]'
-./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=mantic   firefox 2>&1 || true
-
 echo '[noble 24.04]'
 ./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=noble    firefox 2>&1 || true
 
