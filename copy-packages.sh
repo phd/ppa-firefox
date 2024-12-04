273040520ce947997e2713e4a99af0d814d2018a
Piotr Henryk Dabrowski <phd@phd.re>
2024-12-04 20:47:06 +0100

    added Ubuntu 24.10 oracular

--- copy-packages.sh
+++ copy-packages.sh
@@ -8,16 +8,19 @@ cd $(dirname "$0")
 . config
 
 echo '[jammy 22.04]'
-./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=jammy   firefox 2>&1 || true
+./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=jammy    firefox 2>&1 || true
 
 echo '[kinetic 22.10]'
-./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=kinetic firefox 2>&1 || true
+./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=kinetic  firefox 2>&1 || true
 
 echo '[lunar 23.04]'
-./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=lunar   firefox 2>&1 || true
+./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=lunar    firefox 2>&1 || true
 
 echo '[mantic 23.10]'
-./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=mantic  firefox 2>&1 || true
+./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=mantic   firefox 2>&1 || true
 
 echo '[noble 24.04]'
-./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=noble   firefox 2>&1 || true
+./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=noble    firefox 2>&1 || true
+
+echo '[oracular 24.10]'
+./ubuntu-archive-tools/copy-package -y -b --from=ubuntu --from-suite=focal-security --to=~${USER}/ubuntu/firefox --to-suite=oracular firefox 2>&1 || true
